6dcbdedf5a12510f2321dfc6d8d6c0217bdba3b4
yesnoba <zd_1226@163.com>
2015-08-10 10:59:28 +0800

    test

---
+++ 54Bang.Web.Company/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _54Bang.Web.Company
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 54Bang.Web.Company/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_54Bang.Web.Company.MvcApplication" Language="C#" %>
