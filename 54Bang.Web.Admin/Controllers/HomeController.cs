fef5faf9f8d03bc1e17697bf7f6bcf6a920a51ed
yesnoba <zd_1226@163.com>
2015-08-10 12:05:52 +0800

    a

---
+++ 54Bang.Web.Admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _54Bang.Web.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
