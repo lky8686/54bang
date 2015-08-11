7b1af740e34ad5c2b522806ddcc32383d8764df1
yesnoba <zdj1226@gmail.com>
2015-08-11 14:03:40 +0800

    b

---
+++ Bang.Models/PageBarInfoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bang.Models
+{
+    public class PageBarInfoModel
+    {
+        public int RecordCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentIndex { get; set; }
+        public int PageCount
+        {
+            get
+            {
+                if (PageSize == 0)
+                {
+                    return 0;
+                }
+                var count = RecordCount % PageSize;
+                return (RecordCount / PageSize + (count > 0 ? 1 : 0));
+            }
+        }
+    }
+}
