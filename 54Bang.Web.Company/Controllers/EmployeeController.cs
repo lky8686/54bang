eb93d6c8db06d72311572b20c16588c059b92e13
yesnoba <zdj1226@gmail.com>
2015-08-11 11:00:33 +0800

    页面结构

---
+++ 54Bang.Web.Company/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using _54Bang.Web.Company.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _54Bang.Web.Company.Controllers
+{
+    [CompanyAuthorize]
+    /// <summary>
+    /// 员工管理:师傅等等
+    /// </summary>
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 54Bang.Web.Company/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using _54Bang.Web.Company.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _54Bang.Web.Company.Controllers
+{
+    [CompanyAuthorize]
+    /// <summary>
+    /// 订单管理
+    /// </summary>
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
