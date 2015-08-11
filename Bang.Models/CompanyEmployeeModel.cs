fe952da25a90d8cea89df32e0201f9604736c82a
yesnoba <zdj1226@gmail.com>
2015-08-11 17:43:06 +0800

    asdf

---
+++ Bang.Models/CompanyEmployeeModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bang.Models
+{
+    [Serializable]
+    public class CompanyEmployeeModel
+    {
+        //师傅账户	姓名	账户状态	信用等级	技能	注册时间	在线时长	订单数量	师傅详情
+        public string Id { get; set; }
+        public string CompanyId { get; set; }
+        /// <summary>
+        /// 师傅账户
+        /// </summary>
+        public string AccountId { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 账户状态
+        /// </summary>
+        public string Status { get; set; }
+        /// <summary>
+        /// 信用等级
+        /// </summary>
+        public string CreditRating { get; set; }
+        /// <summary>
+        /// 技能
+        /// </summary>
+        public string Skill { get; set; }
+        /// <summary>
+        /// 注册时间
+        /// </summary>
+        public DateTime RegDate { get; set; }
+        /// <summary>
+        /// 在线时长
+        /// </summary>
+        public string OnlineDays { get; set; }
+        /// <summary>
+        /// 订单数量
+        /// </summary>
+        public int OrderCount { get; set; }        
+    }
+}
---
+++ Bang.Models/CompanyOrderModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bang.Models
+{
+    [Serializable]
+    public class CompanyOrderModel
+    {
+        //序号	交易流水号	业务类型	交易时间	业务状态	交易额	手续费	补贴费	交易对方	客户
+        /// <summary>
+        /// 交易流水号
+        /// </summary>
+        public string TransactionSerialNumber { get; set; }
+        /// <summary>
+        /// 交易时间
+        /// </summary>
+        public string TransactionDate { get; set; }
+        /// <summary>
+        /// 业务类型
+        /// </summary>
+        public string ServiceType { get; set; }
+        /// <summary>
+        /// 业务状态
+        /// </summary>
+        public string Status { get; set; }
+        /// <summary>
+        /// 交易额
+        /// </summary>
+        public decimal Total { get; set; }
+        /// <summary>
+        /// 手续费
+        /// </summary>
+        public decimal Poundage { get; set; }
+        /// <summary>
+        /// 补贴费
+        /// </summary>
+        public decimal Bounty { get; set; }
+        /// <summary>
+        /// 交易对方
+        /// </summary>
+        public string Traders { get; set; }
+        /// <summary>
+        /// 客户
+        /// </summary>
+        public string Customer { get; set; }
+    }
+}
