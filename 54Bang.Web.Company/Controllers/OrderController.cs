2333823f6391d1cefce6ab81e5bf2af8c930a10a
yesnoba <zdj1226@gmail.com>
2015-08-11 14:51:19 +0800

    sdfsdf

--- 54Bang.Web.Company/Controllers/OrderController.cs
+++ 54Bang.Web.Company/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using _54Bang.Web.Company.Authentication;
+using Bang.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,25 @@ namespace _54Bang.Web.Company.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ExportExcel(int jobId, string jobTitle, int[] id)//id is candidateid
+        {
+            const string title = "Rank$序号,JobTitle$推荐职位,PhaseText$招聘阶段,Recommender$关联顾问,RecommendDateText$关联时间," +
+                "Name$姓名,Mobile$个人电话,Email$邮箱,GenderText$性别,BirthdayText$出生日期,Age$年龄,SchoolName$学校名称,MajorName$专业名称,DegreeText$学历," +
+                  "Expirence$工作年限,LiveCity$目前居住地,Hukou$户籍,DesiredCity$期望工作地,CurrentCompanyName$最近工作单位,CurrentJobTitle$最近工作职位," +
+                  "IndustryText$所属行业,InterviewComment$评语,Commentor$评语沟通人";
+            var user = UserContext.Current;
+            //var resumeService = new ResumesService();
+            //var dataList = resumeService.Export(user.TenantId, jobId, jobTitle, id);
+            //if (dataList != null && dataList.Count > 0)
+            //{
+            //    var buffer = ExcelHelper.DataToExcel(dataList, title);
+            //    return File(buffer, "application/octet-stream", string.Format("{0:yyyyMMddHHmmssfff}.xls", DateTime.Now));
+            //}
+            ViewBag.MsgCode = 20004;
+            return View("~/Views/Error/Index.cshtml");
         }
 
     }
---
+++ Bang.Common/ExcelHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.HSSF.UserModel;
+using NPOI.HSSF.Util;
+using System.IO;
+
+namespace Bang.Common
+{
+    public class ExcelHelper
+    {
+        /// <summary>
+        /// 导出数据为Excel
+        /// <para>colName为可选，如果指定列名须遵循格式：字段名$列名,字段名$列名,……</para>
+        /// </summary>
+        /// <param name="source">数据</param>
+        /// <param name="colName">列名</param>
+        public static byte[] DataToExcel<T>(IList<Dictionary<string, T>> source, string colName = null)
+        {
+            IList<string> fieldName = new List<string>();
+            IList<string> fieldTitle = new List<string>();
+            ParseName(source, colName, fieldName, fieldTitle);
+
+            HSSFWorkbook workBook = new HSSFWorkbook();
+            HSSFSheet sheet = workBook.CreateSheet();
+            HSSFRow header = sheet.CreateRow(0);
+            HSSFCellStyle cellStyle = workBook.CreateCellStyle();
+            cellStyle.FillBackgroundColor = HSSFColor.BLACK.index;
+            cellStyle.FillPattern = HSSFCellStyle.SOLID_FOREGROUND;
+            HSSFFont font = workBook.CreateFont();
+            font.Boldweight = 600;
+            font.Color = HSSFColor.WHITE.index;
+            cellStyle.SetFont(font);
+
+            //创建列名行
+            for (var i = 0; i < fieldTitle.Count; i++)
+            {
+                var cell = header.CreateCell(i);
+                cell.SetCellValue(fieldTitle[i]);
+                cell.CellStyle = cellStyle;
+            }
+
+            //创建数据行
+            for (var j = 0; j < source.Count; j++)
+            {
+                HSSFRow row = sheet.CreateRow(j + 1);
+                for (var k = 0; k < fieldName.Count; k++)
+                {
+                    if (source[j].ContainsKey(fieldName[k]))
+                    {
+                        row.CreateCell(k).SetCellValue(source[j][fieldName[k]].ToString());
+                    }
+                }
+            }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                workBook.Write(stream);
+                return stream.GetBuffer();
+            }
+        }
+
+        private static void ParseName<T>(IList<Dictionary<string, T>> source, string colName, IList<string> fieldName, IList<string> fieldTitle)
+        {
+            if (string.IsNullOrEmpty(colName)) //如果未指定列名则将字段名作为列名输出
+            {
+                foreach (var k in source[0].Keys)
+                {
+                    fieldName.Add(k);
+                    fieldTitle.Add(k);
+                }
+            }
+            else //析构列名
+            {
+                Array.ForEach(colName.Split(','), s =>
+                {
+                    var t = s.Split('$');
+                    if (t.Length == 1)
+                    {
+                        string t0 = t[0];
+                        fieldName.Add(t0);
+                        fieldTitle.Add(t0);
+                    }
+                    else
+                    {
+                        string t0 = t[0], t1 = t[1];
+                        fieldName.Add(t0);
+                        fieldTitle.Add(t1);
+                    }
+                });
+            }
+        }
+    }
+}
