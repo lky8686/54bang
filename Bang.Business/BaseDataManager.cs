61281fdbaf63c665e34346d5bb16897d0bebf101
yesnoba <zdj1226@gmail.com>
2015-08-11 13:18:08 +0800

    my97date

---
+++ Bang.Business/BaseDataManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bang.Business
+{
+    public class BaseDataManager
+    {
+        public static Dictionary<int, string> GetCityList()
+        {
+            var result = new Dictionary<int, string>();
+            result.Add(1, "北京");
+            result.Add(2, "上海");
+            result.Add(3, "东莞");
+
+            return result;
+        }
+
+        public static Dictionary<int, string> ServiceTypeList()
+        {
+            var result = new Dictionary<int, string>();
+            result.Add(1, "上门洗车");
+            result.Add(2, "清洗油烟机");            
+
+            return result;
+        }
+    }
+}
