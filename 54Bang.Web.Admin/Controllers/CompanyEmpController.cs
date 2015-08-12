07e8fa8a92d011e06c4376a765c3af7690750b17
yesnoba <zdj1226@gmail.com>
2015-08-12 20:09:40 +0800

    all

---
+++ 54Bang.Web.Admin/Controllers/CompanyEmpController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _54Bang.Web.Admin.Controllers
+{
+    public class CompanyEmpController : Controller
+    {
+        //
+        // GET: /CompanyEmp/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 师傅推荐
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Recommend()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 54Bang.Web.Admin/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _54Bang.Web.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+        /// <summary>
+        /// 业务管理==订单管理
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Query()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 交易
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Trade()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TradeQuery()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 结算
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Settlement()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SettlementQuery()
+        {
+            return View();
+        }
+
+    }
+}
