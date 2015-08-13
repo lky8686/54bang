444ef568266c90aa9f40e7a6f382b1d501d0acac
yesnoba <zdj1226@gmail.com>
2015-08-13 09:12:07 +0800

    customer

--- 54Bang.Web.Admin/Controllers/CustomerController.cs
+++ 54Bang.Web.Admin/Controllers/CustomerController.cs
@@ -15,6 +15,18 @@ namespace _54Bang.Web.Admin.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Query(string city,string startDate,string endDate,string customerAccount,int pageIndex)
+        {
+            pageIndex = pageIndex <= 0 ? 1 : pageIndex;
+
+            //todo 
+
+            ViewBag.RecordCount = 93;
+            ViewBag.PageSize = 20;
+            ViewBag.CurrentIndex = pageIndex;
+            return View();
+        }
 
     }
 }
