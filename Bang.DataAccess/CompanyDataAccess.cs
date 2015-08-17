520aedfd305cbba71259f5b39d189e215d61df74
yesnoba <zd_1226@163.com>
2015-08-17 20:15:35 +0800

    company login ok

--- Bang.DataAccess/CompanyDataAccess.cs
+++ Bang.DataAccess/CompanyDataAccess.cs
@@ -12,25 +12,28 @@ namespace Bang.DataAccess
     {
         public static LoginInfoModel VerifyUserLogin(string userName, string password)
         {
-            var sqlString = "select * from where company_status=1 and company_login=@UserName and company_pwd=@password";
-            var userNameParam = OracleHelper.MakeParam("@UserName", userName);
-            var userPasswordParam = OracleHelper.MakeParam("@Password", password);
+            var sqlString = "select * from company_info where company_status=1 and company_login=:UserName and company_pwd=:password";//
+            var userNameParam = OracleHelper.MakeParam("UserName", userName);
+            var userPasswordParam = OracleHelper.MakeParam("Password", password);
             var reader = OracleHelper.ExecuteReader(OracleHelper.OracleConnString, System.Data.CommandType.Text, sqlString, new OracleParameter[]{
             userNameParam, userPasswordParam
             });
-            if (reader.NextResult())
+            //var reader = OracleHelper.ExecuteReader(OracleHelper.OracleConnString, System.Data.CommandType.Text, sqlString, null);
+            if (reader.Read())
             {
                 return new LoginInfoModel
                 {
                     CompanyId = reader.GetString(reader.GetOrdinal("Company_Code")),
                     CompanyName = reader.GetString(reader.GetOrdinal("Company_Name")),
-                    UserName = reader.GetString(reader.GetOrdinal("company_login"))
+                    UserName = reader.GetString(reader.GetOrdinal("company_login")),
+                    Status = 1
                 };
             }
             else
             {
                 return null;
             }
+            //return new LoginInfoModel { Name = "a", UserName = "1", Status = 1 };
         }
 
         public static List<object> GetEmpListBy(string companyId)
