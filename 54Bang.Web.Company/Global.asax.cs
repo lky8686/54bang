e7e11a7503f5c8c91a38940eb42f35422575b0db
yesnoba <zd_1226@163.com>
2015-08-29 14:56:42 +0800

    sql filter

--- 54Bang.Web.Company/Global.asax.cs
+++ 54Bang.Web.Company/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bang.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,48 @@ namespace _54Bang.Web.Company
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void GoSqlErr(string Type, string Data)
+        {
+            string strMsg = "尝试非法字符注入，IP：" + this.Request.ServerVariables["REMOTE_ADDR"] + " 页面："
+                          + this.Request.ServerVariables["URL"].ToLower() + " 方式：" + Type + " 数据：" + Data;
+  
+            this.Response.Write("<script language='javascript'>window.alert('请不要输入非法字符！');location='" + this.Request.ServerVariables["URL"] + "';</script>");
+            this.Response.End();
+        }
+        
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            string strUrl = Request.Url.ToString();
+            if (!strUrl.Contains("umpay/ReceiveUMPayInfo.aspx"))
+            {
+                string strMsg = string.Empty;
+
+                //遍历Post参数，隐藏域除外
+                foreach (string str_t in this.Request.Form)
+                {
+                    if (str_t == "__VIEWSTATE" || str_t == "__EVENTVALIDATION") continue;
+
+                    if (strUrl.ToLower().Contains("/gasagreement.aspx") || strUrl.ToLower().Contains("/bitoagreement.aspx"))
+                    {
+                        if (str_t.ToLower() == "hidcontent") continue;
+                    }
+
+                    if (SqlFilter.Filter(this.Request.Form[str_t].ToString()))
+                    {
+                        GoSqlErr("Post", this.Request.Form[str_t].ToString());
+                    }
+                }
+
+                //遍历Get参数。
+                foreach (string str_t in this.Request.QueryString)
+                {
+                    if (SqlFilter.Filter(this.Request.QueryString[str_t].ToString()))
+                        GoSqlErr("Get", this.Request.QueryString[str_t].ToString());
+                }
+            }
         }
     }
 }
---
+++ Bang.Common/SqlFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bang.Common
+{
+    public static class SqlFilter
+    {
+        /// <summary>
+        /// SQL注入过滤
+        /// </summary>
+        /// <param name="InText">要过滤的字符串</param>
+        /// <returns>如果参数存在不安全字符，则返回true</returns>
+        public static bool Filter(string InText)
+        {
+            //string word = "and|exec|insert|select|delete|update|chr|mid|master|or|truncate|char|declare|join|'|;";            
+            string word = "or |or+|or%20|and|exec|insert|select|delete|update|chr|mid|master|truncate|char|declare|join|'|;|--|cmdshell|drop |drop+|drop%20|alter |alter+|alter%20|create |create+|create%20";
+
+            if (InText == null)
+                return false;
+            foreach (string str_t in word.Split('|'))
+            {
+                if ((InText.ToLower().IndexOf(str_t + " ") > -1) || (InText.ToLower().IndexOf(" " + str_t) > -1) || (InText.ToLower().IndexOf(str_t) > -1))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+    }
+}
